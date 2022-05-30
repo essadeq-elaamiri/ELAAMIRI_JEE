ffad3cf876eaff3723d18f9c444dfa1c61056212
edssadeq <essadeq0701@gmail.com>
2022-05-30 13:22:47 +0100

    operations dtos

---
+++ digitalBanking/ebank-backend/src/main/java/com/elaamiri/ebankbackend/dto/CreditDTO.java
@@ -0,0 +1,9 @@
+package com.elaamiri.ebankbackend.dto;
+
+import lombok.Data;
+@Data
+public class CreditDTO {
+    private String accountId;
+    private double amount;
+    private String description;
+}
